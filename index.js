30d03eed370c6950dac847cc2bfbf0e3866325bd
Matías con tilde <saitam.spatz@gmail.com>
2018-02-21 21:21:09 +0100

    Made it better

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 const TelegramBot = require('node-telegram-bot-api');
-
 const bot = new TelegramBot(process.env.API_KEY, {
   polling: true
 });
 
 const request = require('request');
 
-bot.onText(/(\/start|\/help)/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Este bot manda la palabra del día cuando escribes /palabra");
+bot.onText(/^(\/start|\/help)$/, (msg) => {
+  bot.sendMessage(msg.chat.id, 'Este bot manda la palabra del día cuando escribes /palabra');
 });
 
-bot.onText(/\/palabra/, (msg) => {
-  request('http://dle.rae.es/srv/wotd', function (error, response, body) {
-    let word = body.substring(body.lastIndexOf('">')+2,body.length-5);
-    let link = "dle.rae.es/?" + body.substring(body.lastIndexOf('href="')+6,body.lastIndexOf('">'));
-    bot.sendMessage(msg.chat.id, "La palabra del día es:\n" + word + "\n\n" + link);
+bot.onText(/^\/palabra$/, (msg) => {
+  request('https://dle.rae.es/srv/wotd', function (error, response, body) {
+    const word = /(">)(.+)(\.<\/a>)/.exec(body)[2];
+    const link = /(<a href=")(.+)(">)/.exec(body)[2];
+    bot.sendMessage(msg.chat.id, `La palabra del día es:\n[*${word}*](https://dle.rae.es/?${link})`, {
+      parse_mode: 'Markdown'
+    });
   });
 });
