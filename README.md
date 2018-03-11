b2823021fc4bbd5210f03217da58daffdac8a2cf
Matías con tilde <saitam.spatz@gmail.com>
2018-03-11 02:17:39 +0100

    Now saves all times to file to remember them after restart

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # PalabraDelDiaBot
+Telegram bot that notifies users with the substitutions of the day
+
+## Install
+Get an Telegram API Token by talking to [@BotFather](https://t.me/BotFather)
+
+Create `config.js` file and fill it with
+```js
+module.exports = {
+  API_KEY: '123456789:ABCDEFGHIJKLMOPQRSTUVWXYZabcdefghij'
+}
+```
+and edit the information to yours
+
+Then:
+```bash
+npm install
+```
+
+## Run
+```bash
+npm start
+```
--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const request = require('request');
 const schedule = require('node-schedule');
 let schedules = {};
 
+const fs = require('fs');
+
 bot.onText(/^(\/start|\/help)/, msg => bot.sendMessage(msg.chat.id, 'Este bot manda la palabra del día cuando escribes /palabra. Para recibir la palabra del día cada día, manda /suscribir y /cancelar para cancelar tu suscripción.'));
 
 const sendWord = id => {
@@ -26,24 +28,45 @@ bot.onText(/^\/palabra/, msg => sendWord(msg.chat.id));
 
 bot.onText(/^\/suscribir/, msg => bot.sendMessage(msg.chat.id, 'Vale, ahora manda la hora'));
 
-bot.onText(/[0-2]?[0-9]:[0-5][0-9]/, (msg, match) => {
-  const rule = new schedule.RecurrenceRule();
-  const colonIndex = match.input.indexOf(':');
-  rule.hour = Number(match.input.substr(0, colonIndex));
-  rule.minute = Number(match.input.substr(colonIndex + 1));
-  if (schedules[msg.chat.id]) {
-    schedules[msg.chat.id].reschedule(rule);
-  } else {
-    schedules[msg.chat.id] = schedule.scheduleJob(rule, sendWord.bind(null, msg.chat.id));
-  }
+bot.onText(/^[0-2]?[0-9]:[0-5][0-9]$/, (msg, match) => {
+  let times = JSON.parse(fs.readFileSync('db.json').toString());
+  times[msg.chat.id] = match.input;
+  fs.writeFileSync('db.json', JSON.stringify(times));
+
+  startSchedule(msg.chat.id, match.input);
+
   bot.sendMessage(msg.chat.id, 'Vale, el tiempo se ha registrado');
 });
 
 bot.onText(/^\/cancelar/, msg => {
+  let times = JSON.parse(fs.readFileSync('db.json').toString());
+  if (times[msg.chat.id]) delete times[msg.chat.id];
+  fs.writeFileSync('db.json', JSON.stringify(times));
+
   if (schedules[msg.chat.id]) {
     schedules[msg.chat.id].cancel();
+    delete schedules[msg.chat.id];
     bot.sendMessage(msg.chat.id, 'Vale, ya no recibirás más palabras del día diarias');
   } else {
-    bot.sendMessage(msg.chat.id, 'Ni si quiera te habías suscrito');
+    bot.sendMessage(msg.chat.id, 'Ni si quiera estás suscrito lol');
   }
 });
+
+const startSchedule = (id, time) => {
+  const colonIndex = time.indexOf(':');
+
+  const rule = new schedule.RecurrenceRule();
+  rule.hour = Number(time.substr(0, colonIndex));
+  rule.minute = Number(time.substr(colonIndex + 1));
+
+  if (schedules[id]) {
+    schedules[id].reschedule(rule);
+  } else {
+    schedules[id] = schedule.scheduleJob(rule, sendWord.bind(null, id));
+  }
+};
+
+const times = JSON.parse(fs.readFileSync('db.json').toString());
+for (let id in times) {
+  startSchedule(id, times[id]);
+}
